1b71604f83be365911a447e94a092982b343d419
dolyw <1107224733@qq.com>
2020-01-03 11:30:08 +0800

    更新README.md

--- src/main/java/com/wang/config/WebMvcConfig.java
+++ src/main/java/com/wang/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * 登录接口跨域放开
+ * 全局跨域放开
  *
  * @author wliduo[i@dolyw.com]
  * @date 2019/11/26 14:29
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/user/login").allowedOrigins("*")
+        registry.addMapping("/**").allowedOrigins("*")
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false).maxAge(3600);
     }
